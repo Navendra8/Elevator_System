54a8e482aa7c5d01b8273667eb2fe6eac3411077
Navendra Singh <65842084+Navendra8@users.noreply.github.com>
2023-08-01 03:03:06 +0530

    Delete elevator_system directory

--- elevator_system/elevator_system/urls.py
+++
@@ -1,8 +0,0 @@
-# elevator_system/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('elevators.urls')),
-]
--- elevator_system/elevators/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ElevatorsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'elevators'
